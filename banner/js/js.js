1d18f62e6af4ebd172b9d72f88e814e1ce3638f7
Oleg Lunia <oleglunja2@gmail.com>
2021-09-13 19:05:38 +0300

    refactoring: customized slider

--- banner/js/js.js
+++ banner/js/js.js
@@ -1,22 +1,85 @@
 let sliderItem = document.querySelectorAll('.slider-item');
 let sliderContainer = document.querySelector('.slider-container');
 let isEnabledTest = true;
+let isEnabled = true;
 let i = 0;
 
 function showSlides() {
-	sliderContainer.classList.add('to-left');
+	if(isEnabledTest === true) {
+		isEnabledTest = false;
+		sliderContainer.classList.add('to-left');
 
-	sliderContainer.addEventListener('animationend', function() {
-		this.append(sliderItem[i]);
-		this.classList.remove('to-left');
-	});
-
-	i++;
-
-	if(i == sliderItem.length - 1) {
-		i = 0;
+		sliderContainer.addEventListener('animationend', function() {
+			this.append(sliderItem[i]);
+			this.classList.remove('to-left');
+			isEnabledTest = true;
+		});
+	
+		i++;
+	
+		if(i == sliderItem.length) {
+			i = 0;
+		}
 	}
 
 }
 
 setInterval(showSlides, 5000);
+
+let x;
+let a;
+let b = [];
+
+let s = function (event) {
+	sliderContainer.style.left = `calc(50% - ${x - event.pageX}px)`;
+	a = event.pageX;
+}
+
+function delTransition() {
+	sliderContainer.style.transition = '0s';
+	b = document.querySelectorAll('.slider-item');
+	sliderContainer.style.left = '50%'
+	sliderContainer.append(b[0]);
+	isEnabledTest = true;
+	isEnabled = true;
+}
+
+function rigth() {
+	sliderContainer.style.transition = '0s';
+	b = document.querySelectorAll('.slider-item');
+	sliderContainer.style.left = '50%'
+	sliderContainer.prepend(b[b.length - 1]);
+	isEnabledTest = true;
+	isEnabled = true;
+}
+
+function swipeSlider() {
+
+	sliderContainer.addEventListener('mousedown', (event) => {
+		if(isEnabledTest === true) {
+			isEnabledTest = false;
+			x = event.pageX;
+			sliderContainer.addEventListener('mousemove', s);
+		}
+	});
+
+	sliderContainer.addEventListener('mouseup', () => {
+		if(isEnabled === true) {
+			isEnabled = false;
+			sliderContainer.removeEventListener('mousemove', s);
+			sliderContainer.style.transition = '1s';
+			if(a < x) {
+				sliderContainer.style.left = `calc(50% - ${sliderItem[0].offsetWidth + 5}px)`;
+				setTimeout(delTransition, 2000);
+			} else if(a > x) {
+				sliderContainer.style.left = `calc(50% + ${sliderItem[0].offsetWidth + 5}px)`;
+				setTimeout(rigth, 2000);
+			}
+			
+		}
+		
+	});
+}
+
+
+swipeSlider();
